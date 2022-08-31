f74f7e757be3cba72d3e248c262ce96d92322514
Bishal Pandit <bishalpandit17@gmail.com>
2022-08-31 20:20:26 +0530

    add api_url in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 export const nftAddress = "0xb4083d1775EDCA65d040685130e09a940EA19E6A"
 export const productsAddress = "0xa46d072bB261406c1691085aEDC492e21c61662D"
-export const API_URL = process.env.REACT_APP_API_URL
+export const API_URL = 'https://eth-goerli.g.alchemy.com/v2/J4OS9GQBUgoOTU9o9jOeynB4ssDgMb_B'
